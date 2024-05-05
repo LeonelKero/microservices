84f6cddd529cb2c377f889c87b3f5e003a420d6b
leonel ka <waboleonel@gmail.com>
2024-05-05 20:43:06 +0100

    Compilation error fixed

--- product-service/src/main/java/com/wbt/microservices/core/product/rest/ProductController.java
+++ product-service/src/main/java/com/wbt/microservices/core/product/rest/ProductController.java
@@ -3,20 +3,22 @@ package com.wbt.microservices.core.product.rest;
 import com.wbt.microservices.core.api.product.Product;
 import com.wbt.microservices.core.api.product.ProductService;
 import com.wbt.microservices.core.util.ServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class ProductController implements ProductService {
 
-//    private final ServiceUtil serviceUtil;
-//
-//    public ProductController(ServiceUtil serviceUtil) {
-//        this.serviceUtil = serviceUtil;
-//    }
+    private final ServiceUtil serviceUtil;
+
+    @Autowired
+    public ProductController(ServiceUtil serviceUtil) {
+        this.serviceUtil = serviceUtil;
+    }
 
     @Override
     public Product getProduct(final Long productId) {
-        return new Product(1L, "Macbook Pro 16", 2.5, "serviceUtil.getServiceAddress()");
+        return new Product(1L, "Macbook Pro 16", 2.5, serviceUtil.getServiceAddress());
     }
 }
